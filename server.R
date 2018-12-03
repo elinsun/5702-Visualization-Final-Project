59a8aed0d8a14701604043431a3e462e92999951
elinsun <ys2780@columbia.edu>
2018-12-02 20:00:51 -0500

    add airbnb info Popups

--- server.R
+++ server.R
@@ -4,62 +4,50 @@ server <- function(input, output, session){
   
   # create the background map
   output$mymap <- renderLeaflet({
-    
-    leaflet(df) %>%
+    #dataset <- Dataset()
+    map <- leaflet(data=airbnb[1:10,]) %>% 
+      setView(lng=-74.0445, lat=40.6892, zoom=10) %>%
       addTiles(
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href=" ">Mapbox</a >') %>%
-      addCircleMarkers(radius = 1, color = 'purple', fill = TRUE) %>%
-      setView(lng=-74.0445, lat=40.6892, zoom=10)
-  })
+      addMarkers(lng= ~longitude,lat= ~latitude)
+    })
   
-  # choose Year/Month/Time
-  # selected <- reactive({
-  #   if(!is.null(input$slider_year)){df = df[df$Year == input$slider_year,]}
-  #   if(!is.null(input$slider_month)){df = df[df$Month == input$slider_month,]}
-  #   if(!is.null(input$slider_time)){df = df[df$Time == input$slider_time,]}
+  # # choose Year, Month, time according to radio buton
+  # timescale <- switch(input$radio, Year = 'Year', Month = 'Month', time = 'time')
+  # # paste the chosen value to input 
+  # slider <- paste('input$',timescale)
+  # # get the subset of data we want
+  # filtered <- reactive({df %>% filter(timescale = slider)}) 
+  # # add points
+  # observeEvent(timescale, {
+  # 
+  #   leafletProxy("mymap", data = filtered) %>%
+  #     clearShapes() %>%
+  #     addCircleMakers(lng= ~Longitude,lat= ~Latitude,
+  #                radius=.6, color='purple',
+  #                stroke=FALSE, fillOpacity=0.4)
   # })
 
-  observe({
-    leafletProxy("mymap", data = df) %>%
-      clearShapes() %>%
-      addCircles(~Longitude, ~Latitude, radius=.6, stroke=FALSE, fillOpacity=0.4)
-  })
 
-}
-
-#layerId=~zipcode,
-#fillColor=pal(colorData)
-
-    # if (colorBy == "superzip") {
-    #   # Color and palette are treated specially in the "superzip" case, because
-    #   # the values are categorical instead of continuous.
-    #   colorData <- ifelse(zipdata$centile >= (100 - input$threshold), "yes", "no")
-    #   pal <- colorFactor("viridis", colorData)
-    # } else {
-    #   colorData <- zipdata[[colorBy]]
-    #   pal <- colorBin("viridis", colorData, 7, pretty = FALSE)
-    # }
-    # 
-    # if (sizeBy == "superzip") {
-    #   # Radius is treated specially in the "superzip" case.
-    #   radius <- ifelse(zipdata$centile >= (100 - input$threshold), 30000, 3000)
-    # } else {
-    #   radius <- zipdata[[sizeBy]] / max(zipdata[[sizeBy]]) * 30000
-    # }
-    
+  # Show a popup at the given location
+  airbnbPopup <- function(lat, lng){  
+    selected <- airbnb %>% filter(latitude == lat, longitude == lng)
+    content <- as.character(tagList(
+      tags$h6(as.character(selected$name)),
+      tags$h6("Price: $", as.numeric(selected$price)),
+      sprintf("Include %d bathroom(s) %d bedroom(s)", as.numeric(selected$bathrooms), as.numeric(selected$bedrooms)), tags$br(),
+      sprintf("Accommendate maximum %d people", as.numeric(selected$accommodates))
+      ))
+    leafletProxy("mymap") %>% addPopups(lng, lat, content, layerId = 'airbnbprice')
+    } 
   
-  # # Show a popup at the given location
-  # showZipcodePopup <- function(zipcode, lat, lng) {
-  #   selectedZip <- allzips[allzips$zipcode == zipcode,]
-  #   content <- as.character(tagList(
-  #     tags$h4("Score:", as.integer(selectedZip$centile)),
-  #     tags$strong(HTML(sprintf("%s, %s %s",
-  #                              selectedZip$city.x, selectedZip$state.x, selectedZip$zipcode
-  #     ))), tags$br(),
-  #     sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
-  #     sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
-  #     sprintf("Adult population: %s", selectedZip$adultpop)
-  #   ))
-  #   leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zipcode)
-  # }
+    # When map is clicked, show a popup with city info
+    observe({
+      leafletProxy("mymap") %>% clearPopups()
+      event <- input$mymap_marker_click
+      if (is.null(event)) return()
+      isolate({airbnbPopup(event$lat, event$lng)}) #(event$id, 
+    })
+    
+}
